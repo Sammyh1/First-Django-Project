6f0fbf12940d4bb6592f2522fea8df96191aaf0c
Sammy <sammyimhook@gmail.com>
2016-07-01 08:07:25 +0100

    Finished making the bare functionality of the polls app

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,19 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
+from .models import Question
+
+
 # Create your tests here.
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        Was published reccently should return False for
+        timestamps in the future
+        """
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
